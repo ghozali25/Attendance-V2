770f7508c7e556ce52728ce32bb7a13e0d784127
ghozali25 <ghozali.ahmd25@gmail.com>
2026-04-23 09:54:05 +0700

    update vercel

--- app/Http/Middleware/EnsureSecurityHeaders.php
+++ app/Http/Middleware/EnsureSecurityHeaders.php
@@ -34,7 +34,7 @@ class EnsureSecurityHeaders
         // Content Security Policy
         $cspConfig = [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://cdn.jsdelivr.net",
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://cdn.jsdelivr.net http://attendance-v2-zeta.vercel.app https://attendance-v2-zeta.vercel.app",
             "style-src 'self' 'unsafe-inline' https://unpkg.com https://fonts.googleapis.com https://cdn.jsdelivr.net https://fonts.bunny.net",
             "font-src 'self' https://fonts.gstatic.com https://fonts.bunny.net data:",
             "img-src 'self' data: blob: https: http:",
